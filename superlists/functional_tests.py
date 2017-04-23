d47868b63eb54938ad0254c75f2d82268fcb4d07
Frenchhorn <nyanpasspass@outlook.com>
2017-04-23 14:42:26 +0800

    init unittest framework

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(executable_path='../chromedriver.exe')
+EXECUTABLE_DRIVER = 'phantomjs'
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        if EXECUTABLE_DRIVER == 'chrome':
+            self.browser = webdriver.Chrome(executable_path='../chromedriver.exe')
+        elif EXECUTABLE_DRIVER == 'phantomjs':
+            self.browser = webdriver.PhantomJS(executable_path='../phantomjs.exe')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 查看网站的首页
+        self.browser.get('http://localhost:8000')    
+
+        # 首页的标题有着'To-DO'这个词
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+
