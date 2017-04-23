eacb983f890726ae94062cd52534ef6d71189e7a
Frenchhorn <nyanpasspass@outlook.com>
2017-04-23 15:45:41 +0800

    add new functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 EXECUTABLE_DRIVER = 'phantomjs'
@@ -8,8 +9,9 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         if EXECUTABLE_DRIVER == 'chrome':
             self.browser = webdriver.Chrome(executable_path='../chromedriver.exe')
-        elif EXECUTABLE_DRIVER == 'phantomjs':
+        else:
             self.browser = webdriver.PhantomJS(executable_path='../phantomjs.exe')
+        self.browser.implicitly_wait(2)
 
     def tearDown(self):
         self.browser.quit()
@@ -18,9 +20,31 @@ class NewVisitorTest(unittest.TestCase):
         # 查看网站的首页
         self.browser.get('http://localhost:8000')    
 
-        # 首页的标题有着'To-DO'这个词
+        # 首页的标题和头部有着'To-DO'这个词
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 网站需要输入一个待办事项
+        inputbox = self.browser.find_element_by_tag_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placehodler'), 'Enter a to-do item')
+
+        # 在文本框输入待办事项"Buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+
+        # 按回车键后，页面更新了
+        # 待办事项表格显示"1: Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_tag_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertEqual(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        # 页面又显示了一个文本框，可以输入其它待办事项
+        # 输入"Use peacock feathers to make a fly"
         self.fail('Finish the test!')
+
+        # 页面再次更新，清单显示了这两个待办事项
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
