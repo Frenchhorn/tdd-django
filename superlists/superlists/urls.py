d3f67ca2fa3a9beb44d1863099b336799a55b276
Frenchhorn <nyanpasspass@outlook.com>
2017-04-23 15:07:07 +0800

    add app and first url mapping test

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import lists
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', lists.views.home_page, name='home'),
+    #url(r'^admin/', admin.site.urls),
 ]
