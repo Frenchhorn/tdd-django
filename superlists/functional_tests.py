235da081cecd384e0fe308f23bd8362f18bc66a0
Frenchhorn <nyanpasspass@outlook.com>
2017-04-24 22:34:40 +0800

    refactor functional test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -16,6 +16,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 查看网站的首页
         self.browser.get('http://localhost:8000')    
@@ -35,10 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # 按回车键后，页面更新了
         # 待办事项表格显示"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面又显示了一个文本框，可以输入其它待办事项
         # 输入"Use peacock feathers to make a fly"
@@ -47,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，清单显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 网站需要记住事项清单
         # 需要为每个用户生成唯一的id
