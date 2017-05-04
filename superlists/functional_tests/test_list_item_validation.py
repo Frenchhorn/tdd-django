bcfe5955c4debd41b48e321b56da09bd15e654fa
Frenchhorn <nyanpasspass@outlook.com>
2017-05-04 21:57:58 +0800

    add functional test for list item validation

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -9,13 +9,27 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # 访问首页，不小心提交了空待办事项
         # 输入框为空，按下了回车键
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 首页刷新，显示一个错误消息
         # 提示待办事项不能为空
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 输入一些文字，再次提交，这次成功了
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # 又提交了一个空的待办事项
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 错误消息再次出现
-        self.fail('write me')
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        # 再输一些文字，提交，这次成功了
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Make tea')
